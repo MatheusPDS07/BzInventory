53daf925591549bd6529e6e239f86a026b60e210
Matheus Pereira <116898873+MatheusPDS07@users.noreply.github.com>
2023-09-22 16:21:49 -0300

    Commit V-0.0.3

--- app/src/main/java/com/bz/bzinventory/FragmentActivity.java
+++ app/src/main/java/com/bz/bzinventory/FragmentActivity.java
@@ -31,7 +31,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
